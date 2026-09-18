37b696e13957ce1510864e6a2ee9e5f72664bf40
IlliaBotsRB61 <bots5859@gmail.com>
2026-09-18 11:07:08 +0000

    додав виведення значень х та обчислення у в залежності від х

---
+++ Main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+double x, y;
+
+//введення значення x
+printf("Введіть значення x: \n");
+scanf("%lf", &x);
+
+// обчислення значення y залежно від x
+if (x >= 0 && x < 5) {
+ y = (x - 1) / (x + 1);
+}
+else if (x >= 5 && x < 25) {
+ y = 5*x + 10;
+}
+else if (x >= 100 && x <125){
+ y = (x/ sqrt(x+x*x)-(4*x*x*x));
+}
+
+    return 0;
+}
